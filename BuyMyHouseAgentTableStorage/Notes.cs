01dea4106e459ca6deca32776681d2667b1e5738
soumia <soumiaullrich@gmail.com>
2021-10-21 20:29:57 +0200

    up

---
+++ BuyMyHouseAgentTableStorage/Notes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyMyHouseAgentTableStorage
+{
+    class Notes
+    {
+        // https://bretthargreaves.com/2014/01/11/azure-table-storage-with-repository-pattern/
+    }
+}
